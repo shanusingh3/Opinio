1a59f557652c56f92bd8db9f43c605a665c6de5a
Shanu Singh <iamshanusingh@gmail.com>
2026-07-11 12:03:50 +0530

    prisma client fix

---
+++ apps/backend/prisma.config.js
@@ -0,0 +1,13 @@
+"use strict";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+export default defineConfig({
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: env("DATABASE_URL"),
+    },
+});
+//# sourceMappingURL=prisma.config.js.map
